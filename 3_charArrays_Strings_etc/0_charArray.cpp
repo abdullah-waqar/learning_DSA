03f7fb6ddb2d1f7a2de4f4cd6fa597eaa3c70842
abdullah-waqar <abdullahwaqar803@gmail.com>
2024-12-03 23:03:05 +0500

    Write a function to get the length of character array

--- 3_charArrays_Strings_etc/0_charArray.cpp
+++ 3_charArrays_Strings_etc/0_charArray.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int getLength(char name[])
+{
+    int count = 0;
+   
+    for(int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
 int main()
 {
     // char can only store one single character and if we want to store the multiple characters than we can use the character array
@@ -10,4 +21,6 @@ int main()
     cin >> name;
 
     cout << "Your name is: " << name << endl;
+
+    cout << getLength(name) << endl;
 }
